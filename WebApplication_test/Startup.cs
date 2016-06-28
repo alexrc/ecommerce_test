2b8f9ab283983e70852f8d7a08c85501f62d25b0
alexrc <alexrc.tecnologia@gmail.com>
2016-06-28 15:58:49 -0300

    Criação da webapplication

---
+++ WebApplication_test/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_test.Startup))]
+namespace WebApplication_test
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
